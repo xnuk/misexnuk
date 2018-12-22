f94631d10b841ad906835e6a67eccb81eaaace60
Xnuk Shuman <admin@xnu.kr>
2018-12-23 01:59:28 +0900

    Misexnuk as a cli service

---
+++ app/cli.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import { misexnukPretty } from '../src/misexnuk';
+import { getEnv } from './env'
+
+const { KAKAO_TOKEN } = getEnv()
+
+misexnukPretty(KAKAO_TOKEN)(process.argv[process.argv.length - 1])
+	.then(res =>
+		typeof res === 'string'
+			? console.error(res)
+			: console.log(res.text)
+	)
